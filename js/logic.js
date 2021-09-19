2571b56f28d327454ce35129c7b1769fe4b6e0e6
Shawn McHenry <drraccoony@gmail.com>
2021-09-19 18:11:05 -0400

    Quiz Questions Logic
    Handle wrong and right answers.

--- js/logic.js
+++ js/logic.js
@@ -1,5 +1,6 @@
 document.getElementsByClassName('quizbody')[0].style.visibility = 'hidden';
 
+var question = 0;
 var questions = [
     {q:"Who is Rico?", c0:"Valter", c1:"Rico", c2:"Tamber", c3:"Eppy", answer:"1"},
     {q:"What is Javascript?", c0:"Pizza", c1:"Vehicle", c2:"Scripting Language", c3:"Boat", answer:"2"}
@@ -8,9 +9,13 @@ var questions = [
 function startQuiz() {
     document.getElementsByClassName('quizhome')[0].style.visibility = 'hidden';
     document.getElementsByClassName('quizbody')[0].style.visibility = 'visible';
-    var question = 0;
+    question = 0;
     // Setup the question layout
     document.getElementById("questiontotal").textContent=questions.length;
+    renderQuestion();
+}
+
+function renderQuestion() {
     document.getElementById("questionnumber").textContent=question+1;
     document.getElementById("questionheader").textContent=questions[question].q;
     // Render the quiz answer buttons
@@ -18,8 +23,28 @@ function startQuiz() {
     document.getElementById("questionBtn1").textContent=questions[question].c1;
     document.getElementById("questionBtn2").textContent=questions[question].c2;
     document.getElementById("questionBtn3").textContent=questions[question].c3;
-    
+}
 
+function answer(a) {
+
+    if (a ==  questions[question].answer)
+    { alert("Correct!"); }
+    else
+    { alert("You're wrong!\nAnswer was" + questions[question].answer +"\nYou picked " + a); }
+
+
+    if (question < questions.length-1) {
+    question++ }
+    else {
+        endQuiz();
+    }
+    renderQuestion();
+}
+
+function endQuiz() {
+    alert('Quiz is done!');
+    document.getElementsByClassName('quizhome')[0].style.visibility = 'visible';
+    document.getElementsByClassName('quizbody')[0].style.visibility = 'hidden';
 }
 
 function showScores() {
